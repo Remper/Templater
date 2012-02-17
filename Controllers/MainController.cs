884cfd0774bb483fc04ab0512940c318d8c5db8b
Nechaev <remper@me.com>
2012-02-17 23:30:07 +0400

    Initial

---
+++ Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Templater.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Главная";
+
+            return View();
+        }
+    }
+}
