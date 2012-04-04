eba1f1bcc8aac930febbca4bc57fd038acce0376
Nechaev <remper@me.com>
2012-04-04 14:16:52 +0400

    some code cleanup

--- Misc/Render.cs
+++ Misc/Render.cs
@@ -11,33 +11,26 @@ namespace Templater.Misc
     {
         public static string RenderPartialView(Controller controller, string viewName, object model)
         {
-            controller.ViewData.Model = model;
-            try
-            {
-                using (StringWriter sw = new StringWriter())
-                {
-                    ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(controller.ControllerContext, viewName);
-                    ViewContext viewContext = new ViewContext(controller.ControllerContext, viewResult.View, controller.ViewData, controller.TempData, sw);
-                    viewResult.View.Render(viewContext, sw);
-                    viewResult.ViewEngine.ReleaseView(controller.ControllerContext, viewResult.View);
-
-                    return sw.ToString();
-                }
-            }
-            catch (Exception ex)
-            {
-                return ex.ToString();
-            }
+            return Render.RenderToString(controller, viewName, model, true);
         }
 
         public static string RenderView(Controller controller, string viewName, object model)
+        {
+            return Render.RenderToString(controller, viewName, model, false);
+        }
+
+        private static string RenderToString(Controller controller, string viewName, object model, bool isPartial)
         {
             controller.ViewData.Model = model;
             try
             {
                 using (StringWriter sw = new StringWriter())
                 {
-                    ViewEngineResult viewResult = ViewEngines.Engines.FindView(controller.ControllerContext, viewName, null);
+                    ViewEngineResult viewResult;
+                    if (isPartial)
+                        viewResult = ViewEngines.Engines.FindPartialView(controller.ControllerContext, viewName);
+                    else
+                        viewResult = ViewEngines.Engines.FindView(controller.ControllerContext, viewName, null);
                     ViewContext viewContext = new ViewContext(controller.ControllerContext, viewResult.View, controller.ViewData, controller.TempData, sw);
                     viewResult.View.Render(viewContext, sw);
                     viewResult.ViewEngine.ReleaseView(controller.ControllerContext, viewResult.View);
